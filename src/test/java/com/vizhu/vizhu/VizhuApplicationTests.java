8106e2b9093175101d41e0688c8840fe0d5b2887
yanderbikovmail.ru <yanisderbikov@gmail.com>
2024-03-04 18:35:02 +0800

    initial

---
+++ src/test/java/com/vizhu/vizhu/VizhuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vizhu.vizhu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VizhuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
