3e2da879e8ec28d55d34490b651157e86b72384e
yanderbikovmail.ru <yanisderbikov@gmail.com>
2024-03-05 16:35:16 +0800

    add pid writer

--- src/main/java/com/vizhu/vizhu/VizhuApplication.java
+++ src/main/java/com/vizhu/vizhu/VizhuApplication.java
@@ -2,12 +2,17 @@ package com.vizhu.vizhu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class VizhuApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(VizhuApplication.class, args);
+        SpringApplication app = new SpringApplication(VizhuApplication.class);
+        // Указываете путь к файлу, куда будет записан PID
+        String pidFilePath = "pids/application-node.pid";
+        app.addListeners(new ApplicationPidFileWriter(pidFilePath));
+        app.run(args);
     }
 
 }
